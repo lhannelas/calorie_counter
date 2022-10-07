aa03897299d875cb17825455ec56a0edadc5e4f2
lhannelas <lhannelas@gmail.com>
2022-10-07 23:38:12 +0400

    final

---
+++ counter/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class CounterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'counter'
+
+    def ready(self): 
+        import counter.signals
